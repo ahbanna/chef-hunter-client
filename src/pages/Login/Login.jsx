31957ae721eebd67dff4571fac931aa682108b6c
Al Hasanul Banna <tutul5800@gmail.com>
2023-05-04 03:43:17 +0600

    login page design

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import "./Login.css";
 
 const Login = () => {
   const { signIn } = useContext(AuthContext);
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
   const handleLogin = (event) => {
     event.preventDefault();
@@ -17,45 +23,54 @@ const Login = () => {
       .then((result) => {
         const loggedUser = result.user;
         console.log(loggedUser);
+        event.target.reset();
+        setSuccess("Log in Successfully");
+        setError(""); // remove prvious error
       })
       .catch((error) => {
-        console.log(error);
+        setError("Invalid email or password");
       });
   };
 
   return (
-    <div className="container w-25 mx-auto">
-      <h2>Login Your Account</h2>
-      <Form onSubmit={handleLogin}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            required
-          />
-        </Form.Group>
+    <div className="login-area">
+      <Header></Header>
+      <div className="container w-25 mx-auto login-content">
+        <h2>Login Your Account</h2>
+        <Form onSubmit={handleLogin}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              required
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-        <br />
-        <Form.Text className="text-secondary">
-          Don't have an account? <Link to="/register">Register</Link>
-        </Form.Text>
-        <Form.Text className="text-success"></Form.Text>
-        <Form.Text className="text-danger"></Form.Text>
-      </Form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+          <br />
+          <Form.Text className="text-secondary">
+            Don't have an account? <Link to="/login">login</Link>
+          </Form.Text>
+          <Form.Text className="text-success"></Form.Text>
+          <Form.Text className="text-danger"></Form.Text>
+          <p className="text-success">{success}</p>
+          <p className="text-danger">{error}</p>
+        </Form>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
